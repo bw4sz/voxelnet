b647fdae0ffbc3fd6b475284c5acf28e4e3bb67e
bw4sz <benweinstein2010@gmail.com>
2018-04-17 14:27:25 -0700

    use the module system for tensorboard

--- Start_TensorBoard.sh
+++ Start_TensorBoard.sh
@@ -11,9 +11,9 @@
 #SBATCH --output=/home/b.weinstein/logs/tensorboard.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/tensorboard.err
 
-source activate voxelnet
+ml tensorflow
 
 port=$(shuf -i 20000-30000 -n 1)
 echo -e "\nSSH tunnel command: ssh -NL 8080:$(hostname):${port} ${USER}@hpg2.rc.ufl.edu"
 echo -e "\nLocal URI: http://localhost:8080"
-tensorboard --logdir /home/b.weinstein/voxelnet/log
+launch_tensorflow tensorboard --logdir /home/b.weinstein/voxelnet/log

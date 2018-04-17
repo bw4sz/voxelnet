2f8d42dd79b61b95a803abdd72212246baacbdba
bw4sz <benweinstein2010@gmail.com>
2018-04-17 16:17:04 -0400

    can't get log dirs

--- SubmitGPU.sh
+++ SubmitGPU.sh
@@ -8,12 +8,12 @@
 #SBATCH --ntasks=1                 # Number of MPI ranks
 #SBATCH --cpus-per-task=1            # Number of cores per MPI rank
 #SBATCH --time=12:00:00       #Time limit hrs:min:sec
-#SBATCH --output=logs/Voxelnet.out   # Standard output and error log
-#SBATCH --error=logs/Voxelnet.err
+#SBATCH --output=/home/b.weinstein/logs/Voxelnet.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/Voxelnet.err
 #SBATCH --distribution=cyclic:cyclic
 #SBATCH --mem-per-cpu=2000
 #SBATCH --partition=hpg2-gpu
-#SBATCH --gres=gpu:tesla:2
+#SBATCH --gres=gpu:tesla:1
 
 #activate conda environment
 source activate voxelnet

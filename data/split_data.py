5fb723c8ff69a05f963acb3cea8c7d42c404b665
bw4sz <benweinstein2010@gmail.com>
2018-04-17 10:35:39 -0700

    split data file to match train and val datasets

---
+++ data/split_data.py
@@ -0,0 +1,29 @@
+import pandas
+from shutil import copyfile
+
+train=pandas.read_table("/ImageSets/train.txt",header=None,dtype="str")
+validation=pandas.read_table("/ImageSets/val.txt",header=None,dtype="str")
+
+train.columns= ["filename"]
+validation.columns= ["filename"]
+
+#cropped data directory
+original_directory="/ufrc/ewhite/b.weinstein/"
+destination_directory="/ufrc/ewhite/b.weinstein/voxelnet_split/"
+
+train.columns= ["filename"]
+for index,row in train.iterrows():
+    pc="training/velodyne/%s.bin" % row.filename
+    im="training/image_2/%s.png" % row.filename
+    label="training/label_2/%s.txt" % row.filename
+    
+    for f in [pc,im,label]:
+        copyfile(original_directory + f,destination_directory+f)
+    
+for index,row in validation.iterrows():
+    pc="validation/velodyne/%s.bin" % row.filename
+    im="validation/image_2/%s.png" % row.filename
+    label="validation/label_2/%s.txt" % row.filename
+    
+    for f in [pc,im,label]:
+        copyfile(original_directory + f,destination_directory+f)    

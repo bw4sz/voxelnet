1abc980cad9957f8ee14a5e874f5e830c99f61a7
bw4sz <benweinstein2010@gmail.com>
2018-04-17 13:15:41 -0700

    tensorboard submit screen

--- SubmitGPU.sh
+++ SubmitGPU.sh
@@ -3,23 +3,20 @@
 #SBATCH --mail-type=FAIL,END               # Mail events (NONE, BEGIN, END, FAIL, AL$
 #SBATCH --mail-user=ben.weinstein@weecology.org   # Where to send mail
 #SBATCH --account=ewhite
-#SBATCH --qos=ewhite-b
 
 #SBATCH --ntasks=1                 # Number of MPI ranks
 #SBATCH --cpus-per-task=1            # Number of cores per MPI rank
 #SBATCH --time=12:00:00       #Time limit hrs:min:sec
-#SBATCH --output=logs/Voxelnet.out   # Standard output and error log
-#SBATCH --error=logs/Voxelnet.err
+#SBATCH --output=/home/b.weinstein/logs/Voxelnet.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/Voxelnet.err
 #SBATCH --distribution=cyclic:cyclic
 #SBATCH --mem-per-cpu=2000
 #SBATCH --partition=hpg2-gpu
-#SBATCH --gres=gpu:tesla:2
+#SBATCH --gres=gpu:tesla:1
 
-#activate conda environment
+#activate conda environmentthing
 source activate voxelnet
 
-cd voxelnet
-
-python train.py --alpha 1 --beta 10 --vis
+python /home/b.weinstein/voxelnet/train.py --alpha 1 --beta 10 --vis
 
 date
